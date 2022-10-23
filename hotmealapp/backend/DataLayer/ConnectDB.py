88c56396d1d2264a5ad8d8d598f0410dd0723284
z5272870 <114211487+z5272870@users.noreply.github.com>
2022-10-24 01:17:18 +1100

    Update ConnectDB.py
    delete some useless code

--- hotmealapp/backend/DataLayer/ConnectDB.py
+++ hotmealapp/backend/DataLayer/ConnectDB.py
@@ -1,11 +1,5 @@
 import pymysql
 
-# def connectDb():
-#     db = pymysql.connect(host='localhost',
-#                          user='root',
-#                          password='L69111a555!',
-#                          database='sys')
-#     return db
 def connectDb():
     db = pymysql.connect(host='database-2.cihwwseytivg.ap-southeast-2.rds.amazonaws.com',
                          user='admin',
