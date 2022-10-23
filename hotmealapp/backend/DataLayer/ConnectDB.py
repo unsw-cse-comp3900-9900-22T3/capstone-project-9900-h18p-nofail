d373a8cd7479c01b11e4297696ddd072091ff7b4
z5272870 <114211487+z5272870@users.noreply.github.com>
2022-10-24 01:09:42 +1100

    Update ConnectDB.py
    connect database to aws,
    then update the host name, user and password

--- hotmealapp/backend/DataLayer/ConnectDB.py
+++ hotmealapp/backend/DataLayer/ConnectDB.py
@@ -1,11 +1,15 @@
 import pymysql
 
-# from flask_mysqldb import MySQL
-
+# def connectDb():
+#     db = pymysql.connect(host='localhost',
+#                          user='root',
+#                          password='L69111a555!',
+#                          database='sys')
+#     return db
 def connectDb():
-    db = pymysql.connect(host='localhost',
-                         user='root',
-                         password='12345678',
+    db = pymysql.connect(host='database-2.cihwwseytivg.ap-southeast-2.rds.amazonaws.com',
+                         user='admin',
+                         password='liuyiyang',
                          database='sys')
     return db
 
