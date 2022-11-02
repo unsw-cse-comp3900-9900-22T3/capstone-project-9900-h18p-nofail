1022951feb6cbd35126ee69cfaece970d47aab92
z5272870 <114211487+z5272870@users.noreply.github.com>
2022-11-02 15:59:53 +1100

    Update server.py
    add cloud photo

--- hotmealapp/backend/DataLayer/server.py
+++ hotmealapp/backend/DataLayer/server.py
@@ -860,6 +860,44 @@ def search_user():
 def logout():
     session.pop('username', None)
     return jsonify({'status': 'success', 'message': 'You have successfully logged out!'})
+
+
+
+from flask_qiniustorage import Qiniu
+
+QINIU_ACCESS_KEY = 'ia2_Xh4kW0_6fK5cvrACV6ICkv_Wvjo1tqmvM5OS'
+QINIU_SECRET_KEY = 'sfN9dqTYsi5Sm4K3IKJguIPauzz6zysMzbDrzpdw'
+QINIU_BUCKET_NAME = 'no-fail-image-bucket'
+QINIU_BUCKET_DOMAIN = 'rkipgq0yn.sabkt.gdipper.com'
+
+# app = Flask(__name__)
+app.config.from_object(__name__)
+qiniu_store = Qiniu(app)
+# 或者
+# qiniu_store = Qiniu()
+# qiniu_store.init_app(app)
+
+# 保存文件到七牛
+@app.route('/save')
+def save(data,filename):
+    # data = 'data to save'
+    # filename = 'filename'
+    print('here')
+    ret, info = qiniu_store.save(data, filename)
+    return str(ret)
+
+# 删除七牛空间中的文件
+@app.route('/delete')
+def delete():
+    filename = 'filename'
+    ret, info = qiniu_store.delete(filename)
+    return str(ret)
+
+# 根据文件名获取对应的公开URL
+@app.route('/url')
+def url():
+    filename = 'filename'
+    return qiniu_store.url(filename)
 #######End of Business Layer#######======================================================
 
 #######Datalayer########================================================================
